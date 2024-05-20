5a7f1f3fcf5f37dedddcc779d8e90ad320bf1f34
Rene Donkers <rene@lotsofpixels.io>
2024-05-20 17:50:06 +0000

    Beta release for testing

--- view/frontend/web/js/free-shipping-banner.js
+++ view/frontend/web/js/free-shipping-banner.js
@@ -1,17 +1,66 @@
 define([
-    'uiComponent'
-], function(
-    Component )
-    {
-        'use strict';
+    'uiComponent',
+    'Magento_Customer/js/customer-data',
+    'underscore',
+    'knockout'
+], function (
+    Component,
+    customerData,
+    _,
+    ko
+) {
+    'use strict';
 
-        return Component.extend({
-            defaults: {
-                message: 'Free shipping message' + '!!'
-            },
-            initialize: function (){
-                this._super();
-                console.log(this.message);
+    return Component.extend({
+        defaults: {
+            freeShippingThreshold: 100,
+            subtotal: 0.00,
+            template: 'Lotsofpixels_PromotionBar/free-shipping-banner',
+            tracks: {
+                subtotal: true
             }
-        });
-    })
+        },
+        initialize: function () {
+            this._super();
+
+            var self = this;
+            var cart = customerData.get('cart');
+
+            customerData.getInitCustomerData().done(function () {
+                if (!_.isEmpty(cart()) && !_.isUndefined(cart().subtotalAmount)) {
+                    self.subtotal = parseFloat(cart().subtotalAmount);
+                }
+            });
+
+            cart.subscribe(function (cart) {
+                if (!_.isEmpty(cart) && !_.isUndefined(cart.subtotalAmount)) {
+                    self.subtotal = parseFloat(cart.subtotalAmount);
+                }
+            });
+
+            self.message = ko.computed(function () {
+
+                if (_.isUndefined(self.subtotal) || self.subtotal === 0) {
+
+                    return self.messageDefault;
+                }
+
+
+                if (self.subtotal > 0 && self.subtotal < self.freeShippingThreshold) {
+                    var subtotalRemaining = self.freeShippingThreshold - self.subtotal;
+                    var formattedSubtotalRemaining = self.formatCurrency(subtotalRemaining);
+                    return self.messageItemsInCart.replace('$XXX', formattedSubtotalRemaining);
+                }
+
+
+                if (self.subtotal >= self.freeShippingThreshold) {
+                    return self.messageFreeShipping;
+                }
+
+            });
+        },
+        formatCurrency: function (value) {
+            return value.toFixed(2);
+        }
+    });
+})
