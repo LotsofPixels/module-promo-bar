abddfcf3c7ed62bf67ba939845345ec91340b709
Rene Donkers <rene@lotsofpixels.io>
2024-05-20 05:17:36 +0000

    Setup framework

---
+++ view/frontend/web/js/free-shipping-banner.js
@@ -0,0 +1,17 @@
+define([
+    'uiComponent'
+], function(
+    Component )
+    {
+        'use strict';
+
+        return Component.extend({
+            defaults: {
+                message: 'Free shipping message' + '!!'
+            },
+            initialize: function (){
+                this._super();
+                console.log(this.message);
+            }
+        });
+    })
